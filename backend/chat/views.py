1ff8536c4217e78b337725a5b79dba629091efdb
Otero <oteronickbrian@gmail.com>
2024-10-17 02:08:27 +0300

    refactor code

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 
@@ -11,5 +10,5 @@ def course_chat_room(request, course_id):
     except:
         # user is not a student of the course or course does not exist
         return HttpResponseForbidden()
-    return render(request, "chat/room.html", {"course": course})
-    
+
+    return JsonResponse({"course": {"id": course.id, "name": course.name}})
--- backend/courses/api/views.py
+++ backend/courses/api/views.py
@@ -12,7 +12,6 @@ from courses.api.permissions import IsEnrolled
 from courses.api.serializers import CourseWithContentsSerializer
 
 
-
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
@@ -37,19 +36,23 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    @action(detail=True,
-            methods=['post'],
-            authentication_classes=[BasicAuthentication],
-            permission_classes=[IsAuthenticated])
+    @action(
+        detail=True,
+        methods=["post"],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
     def enroll(self, request, *args, **kwargs):
         course = self.get_object()
         course.students.add(request.user)
-        return Response({'enrolled': True})
+        return Response({"enrolled": True})
 
-    @action(detail=True,
-            methods=['get'],
-            serializer_class=CourseWithContentsSerializer,
-            authentication_classes=[BasicAuthentication],
-            permission_classes=[IsAuthenticated, IsEnrolled])
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
     def contents(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
