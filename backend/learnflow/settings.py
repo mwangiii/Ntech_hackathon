ac84368bfecbf82facadc8842b8ec3e81faa8a45
Otero <oteronickbrian@gmail.com>
2024-10-17 12:53:28 +0300

    feat add:notifications

--- backend/learnflow/settings.py
+++ backend/learnflow/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "chat",
     "channels",
+    "notifications",
 ]
 
 MIDDLEWARE = [
---
+++ backend/notifications/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    content = models.TextField()
+    type = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User:{self.user} / Type:{self.type}"
